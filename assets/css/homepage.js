20a6173a2c4e590c04561a3f3d8b7fddcc470606
nosremetnarg <grant.alex.emerson@gmail.com>
2020-07-06 12:41:49 -0500

    github api username function

--- assets/css/homepage.js
+++ assets/css/homepage.js
@@ -1,8 +1,19 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+// getUserRepos("microsoft");
+getUserRepos("nosremetnarg");
 
-console.log(response.location);
-console.log(response.followers);
+// getUserRepos("facebook");
+
+
+// console.log(response.location);
